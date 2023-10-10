219143c0a6dfd2d49af3438d5c75b98c42a13818
Evan Kirkiles <kirkilese@gmail.com>
2023-10-09 20:52:36 -0400

    Added GA4 metrics.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -12,10 +12,25 @@ import SkipLink from '@/components/SkipLink';
 import { PropsWithChildren } from 'react';
 import Footer from '@/app/(main)/_components/Footer';
 import s from './Layout.module.scss';
+import Script from 'next/script';
+import { GA4_TAG } from '@/env';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA4_TAG}`}
+      />
+      <Script id="google-analytics">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${GA4_TAG}');
+    `}
+      </Script>
       <Header />
       <main className={s.main} tabIndex={-1}>
         {children}
