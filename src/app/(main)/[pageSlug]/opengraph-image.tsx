108f19aff21f02a39b7bc24d6f20bb83822463cf
Evan Kirkiles <kirkilese@gmail.com>
2026-04-09 15:39:55 -0400

    Try to reduce size of og endpoint

--- src/app/(main)/[pageSlug]/opengraph-image.tsx
+++ src/app/(main)/[pageSlug]/opengraph-image.tsx
@@ -5,7 +5,7 @@
  * 2023 Digital Portfolio
  */
 
-import { ImageResponse } from 'next/og';
+import { ImageResponse } from '@vercel/og';
 
 export const runtime = 'edge';
 export const contentType = 'image/png';
@@ -14,76 +14,27 @@ export const size = {
   height: 630,
 };
 
-export default async function Image({
-  params: { pageSlug },
-}: {
-  params: { pageSlug: string };
-}) {
+export default async function Image() {
   return new ImageResponse(
-    (
-      <div
-        style={{
-          fontFamily: 'Arial',
-          fontSize: 180,
-          background: 'black',
-          color: 'white',
-          width: '100%',
-          height: '100%',
-          // paddingTop: '1%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          // padding: '3rem',
-          fontWeight: '700',
-        }}
-      >
-        <div>EWK</div>
-        {/* <div
-          style={{
-            display: 'flex',
-            position: 'relative',
-            flexDirection: 'column',
-            borderTop: '1px solid white',
-            borderBottom: '1px solid white',
-            flex: '1',
-            padding: '1rem 0',
-            boxSizing: 'border-box',
-          }}
-        >
-          Evan Kirkiles
-          <br />
-          <span style={{ fontWeight: '400', fontSize: 36 }}>
-            Art / Software / Design / About
-          </span>
-          <div style={{ flex: 1 }}></div>
-          <span
-            style={{
-              fontWeight: '400',
-              fontSize: 36,
-              letterSpacing: '0.1em',
-              color: '#c0c0c0',
-            }}
-          >
-            https://evankirkiles.com
-          </span>
-          <div
-            style={{
-              position: 'absolute',
-              top: '0',
-              right: '0',
-              bottom: '0',
-              height: '84.5vh',
-              width: '84.5vh',
-              boxSizing: 'border-box',
-              // width: '85vh',
-              borderRadius: '100%',
-              border: '1px solid white',
-            }}
-          ></div>
-        </div> */}
-      </div>
-    ),
+    <div
+      style={{
+        fontFamily: 'Arial',
+        fontSize: 180,
+        background: 'black',
+        color: 'white',
+        width: '100%',
+        height: '100%',
+        // paddingTop: '1%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // padding: '3rem',
+        fontWeight: '700',
+      }}
+    >
+      <div>EWK</div>
+    </div>,
     {
       ...size,
       fonts: [
